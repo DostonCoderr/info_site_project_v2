9bbab6fc10a39f23d65b5be43dedb84809cb7d6f
DostonCoderr <otkurovdoston69@gmail.com>
2025-07-19 15:48:10 +0500

    add Ceo

--- info_site/urls.py
+++ info_site/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
 # добавляем это для загрузки файлов в режиме разработки
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
 
 urlpatterns = [
     path('bobur/', admin.site.urls),
-    path('', include('pages.urls')),  # твое приложение
+    path('', include('pages.urls')),  
+    re_path(r'^google9f3904655a724430\.html$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),# твое приложение
 ]
 
 # разрешаем отдачу медиафайлов при DEBUG = True
